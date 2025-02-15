f365513ae3cd450d222f04aad5111513c86c74ef
yemiwebby <yemiwebby@gmail.com>
2025-02-15 00:40:50 +0000

    fixing POST in progress

--- app/Http/Controllers/CircleCIController.php
+++ app/Http/Controllers/CircleCIController.php
@@ -10,31 +10,31 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CircleCIController extends Controller
 {
-    public function getAllNotifications()
-    : JsonResponse {
+//    public function getAllNotifications()
+//    : JsonResponse {
+//
+//        return response()->json(WebhookNotification::all());
+//    }
 
-        return response()->json(WebhookNotification::all());
-    }
-
-    public function handleNotification(Request $request)
-    : JsonResponse {
-
-        return response()->json([$request->headers->all(), $request->all()]);
+//    public function handleNotification(Request $request)
+//    : JsonResponse {
+//
+//        return response()->json([$request->headers->all(), $request->all()]);
 
 //        CircleCINotificationHelper::handle($request);
 //
 //        return response()
 //            ->json(null, Response::HTTP_NO_CONTENT);
+//    }
+
+
+    public function handleNotification(Request $request)
+    {
+        return response()->json(['message' => 'POST received!']);
     }
 
-
-//    public function handleNotification(Request $request)
-//    {
-//        return response()->json(['message' => 'POST received!']);
-//    }
-//
-//    public function getAllNotifications()
-//    {
-//        return response()->json(['message' => 'GET request received!']);
-//    }
+    public function getAllNotifications()
+    {
+        return response()->json(['message' => 'GET request received!']);
+    }
 }
